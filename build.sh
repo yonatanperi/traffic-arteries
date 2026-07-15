78f13377acd31b47df35a038ac69e343c25c7ab5
yonatanperi <yonatanperi333@gmail.com>
2026-07-15 16:27:48 +0300

    refactor: move build script to backend

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Render build script. manage.py lives under backend/.
-set -o errexit
-
-pip install -r backend/requirements.txt
-python backend/manage.py collectstatic --noinput
-python backend/manage.py migrate
